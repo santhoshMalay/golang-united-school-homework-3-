8e3400920d11a6d81f3fa0991b2def9bb59c4d23
smalay <smalay@slb.com>
2022-06-09 10:27:34 +0530

    Empty Array Check

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,7 +1,9 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
+	if len(input) < 1 {
+		return 0.0
+	}
 	var sum float32 = 0.0
 	for _, val := range input {
 		sum = sum + val
