dde1a17c6bdb446c6dddfb01ea49a8ac7b88e762
smalay <smalay@slb.com>
2022-06-09 12:08:12 +0530

    Reversing the slice

--- task02-slice.go
+++ task02-slice.go
@@ -2,5 +2,10 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	return
+	for i, j := 0, len(input)-1; i < j; i, j = i+1, j-1 {
+		temp := input[i]
+		input[i] = input[j]
+		input[j] = temp
+	}
+	return input
 }
